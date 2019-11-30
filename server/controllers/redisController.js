1fd44b57ecaf7ef382d2d5a53186cad3a62d7cc8
kendall <xxpker222@gmail.com>
2019-11-30 11:34:13 -0800

    refactored controller for verifyUser, it was breaking and crashing the controller when an invalid username/password was submitted - not allowing for further attempts -, also got rid of unecessary console logs and cleaned up small areas on the front end

--- server/controllers/redisController.js
+++ server/controllers/redisController.js
@@ -2,14 +2,11 @@ const redisController = {};
 const client = require('../databases/redis');
 
 redisController.setData = (req, res, next) => {
-  console.log('inside redis controller', req.body)
   client.set(req.body.username, JSON.stringify(req.body.interests), (err, reply) => {
-    console.log('inside of set', reply)
     return next();
   })
 }
 redisController.getData = (req, res, next) => {
-  console.log('inside get data', res.locals)
   client.get(res.locals.username, (err, reply) => {
     if (err) return next(err);
     res.locals.reply = JSON.parse(reply);
